d495031d0975cdef6231c42edffd3f9ad92905f8
Greg <greg@helding.net>
2025-01-03 10:43:38 -0600

    Feature Add: If captions are used in Obsidian, they will be added to markdown link as a Caption and Alt Text. This required moving the order of the image processing.

--- imgproc.py
+++ imgproc.py
@@ -23,22 +23,8 @@ for filename in os.listdir(posts_dir):
         if not os.path.exists(post_dir):
             os.mkdir(post_dir)
 
-        # Step 3: Find and format image links and copy images to post_dir
-        # Find all image links in the format ![Image Description](/images/Pasted%20image%20...%20.png, etc)
-        images = re.findall(r'!\[\[([^]]*\.(?:png|jpg|jpeg|gif|webp))\]\]', content)
-
-        # Replace image links; ensure URLs are correctly formatted; remove cover image and thumbnail image links
-        for image in images:
-            # Prepare the Markdown-compatible link with %20 replacing spaces
-            markdown_image = f"![Image Description]({image.replace(' ', '%20')})"
-            content = content.replace(f"![[{image}]]", markdown_image)
-            # Copy the image to the post_dir
-            image_source = os.path.join(attachments_dir, image)
-            if os.path.exists(image_source):
-                shutil.copy(image_source, post_dir)
-
-        #Step 4: Find cover image link, remove link, and copy image to post_dir
-        cover_image = re.findall(r'!\[\[([^]]*\.(?:png|jpg|jpeg|gif|webp))\|cover\]\]', content)
+        # Step 3: Find cover image link, remove link, and copy image to post_dir
+        cover_image = re.findall(r'!\[\[([^|\]]*\.(?:png|jpg|jpeg|gif|webp))\|cover\]\]', content)
         for image in cover_image:
             # Replace the cover image link with an empty string
             content = re.sub(f"^!\\[\\[{image}\\|cover\\]\\]\\n", "", content, flags=re.MULTILINE)
@@ -49,8 +35,8 @@ for filename in os.listdir(posts_dir):
             if os.path.exists(image_source):
                 shutil.copy(image_source, new_filepath)
 
-        #Step 5: Find thumbnail image link, remove link, and copy image to post_dir
-        thumbnail_image = re.findall(r'!\[\[([^]]*\.(?:png|jpg|jpeg|gif|webp))\|thumbnail\]\]', content)
+        # Step 4: Find thumbnail image link, remove link, and copy image to post_dir
+        thumbnail_image = re.findall(r'!\[\[([^|\]]*\.(?:png|jpg|jpeg|gif|webp))\|thumbnail\]\]', content)
         for image in thumbnail_image:
             # Replace the thumbnail image link with an empty string
             content = re.sub(f"^!\\[\\[{image}\\|thumbnail\\]\\]\\n", "", content, flags=re.MULTILINE)
@@ -60,6 +46,22 @@ for filename in os.listdir(posts_dir):
             image_source = os.path.join(attachments_dir, image)
             if os.path.exists(image_source):
                 shutil.copy(image_source, new_filepath)
+        
+        # Step 5: Find and format remaining image links (that are not cover or thumbnail) and copy images to post_dir
+        # Find all image links in the Obsidian Format '![[image.png|text]]', where text is the caption for the image
+        
+        images = re.findall(r'!\[\[([^|\]]*\.(?:png|jpg|jpeg|gif|webp))(?:\|([^]]*))?\]\]', content)
+
+        # Replace image links; ensure URLs are correctly formatted; remove cover image and thumbnail image links
+        for image, text in images:
+            # Prepare the Markdown-compatible link with %20 replacing spaces
+            markdown_image = f'![{text}]({image.replace(" ", "%20")} "{text}")'
+            content = content.replace(f"![[{image}|{text}]]", markdown_image)
+            # Copy the image to the post_dir
+            image_source = os.path.join(attachments_dir, image)
+            if os.path.exists(image_source):
+                shutil.copy(image_source, post_dir)
+
 
         # Step 6: Write the updated content back to the markdown file
         with open(filepath, "w") as file:
